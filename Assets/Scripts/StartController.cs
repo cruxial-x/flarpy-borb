ead97440ed4c85f696176de431503d80550890d2
jseymour93 <seymour.jordan93@gmail.com>
2024-03-01 22:06:34 -0600

    i forgot

--- Assets/Scripts/StartController.cs
+++ Assets/Scripts/StartController.cs
@@ -18,7 +18,7 @@ public class StartScreen : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetMouseButtonDown(0))
         {
             StartGame();
         }
