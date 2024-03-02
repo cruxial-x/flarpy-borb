5fc30e8dad63e8103d915c865005dbfc8ed7bcbf
jseymour93 <seymour.jordan93@gmail.com>
2024-03-01 22:55:42 -0600

    actually fix build

--- Assets/Scripts/PipeController.cs
+++ Assets/Scripts/PipeController.cs
@@ -41,7 +41,7 @@ public class PipeController : MonoBehaviour
         Vector2 spawnPosition = new(transform.position.x, randomY);
         GameObject newCloud = Instantiate(cloudPrefab, spawnPosition, Quaternion.identity);
         Collider2D cloudCollider = newCloud.GetComponent<Collider2D>();
-        Collider2D playerCollider = GameObject.Find("Bird").GetComponent<Collider2D>();
+        Collider2D playerCollider = GameObject.Find("Bird")?.GetComponent<Collider2D>();
         if (cloudCollider != null && playerCollider != null)
         {
             Physics2D.IgnoreCollision(playerCollider, cloudCollider);
