2efbc2965c64604ad6315b5900bb0d07ded85c03
jseymour93 <seymour.jordan93@gmail.com>
2024-03-02 02:39:41 -0600

    balance

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public float speed = 1.0f;
+    private GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.Find("Bird");
     }
 
     // Update is called once per frame
@@ -19,10 +20,19 @@ public class Enemy : MonoBehaviour
     }
     void MoveTowardsPlayer()
     {
-        GameObject player = GameObject.Find("Bird");
-        Vector3 direction = player.transform.position - transform.position;
+        Vector3 targetPosition = player.transform.position;
+        targetPosition.x -= 2; // Adjust this value to change how far to the left of the player the enemy will target
+        Vector3 direction = targetPosition - transform.position;
         direction.Normalize();
-        transform.position += speed * Time.deltaTime * direction;
+        // If player is to the right of the enemy, just move left
+        if (player.transform.position.x > transform.position.x)
+        {
+            transform.position += speed * Time.deltaTime * Vector3.left;
+        }
+        else
+        {
+            transform.position += speed * Time.deltaTime * direction;
+        }
     }
     void RotateMenacingly()
     {
